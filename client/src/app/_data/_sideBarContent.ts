41eca7b741615a61690b1aa37b281df2be8a84f3
eric-programming <eric09279@outlook.com>
2020-10-26 21:10:44 -0700

    updates sideboar

--- client/src/app/_data/_sideBarContent.ts
+++ client/src/app/_data/_sideBarContent.ts
@@ -1,31 +1,43 @@
+import { Role } from '../_model/_Enum/Role';
 import {
   _dashboardStats_route,
   _editAccount,
   _editCertification_route,
+  _maintrainer,
+  _mainUser,
   _myMessages_route,
 } from './_route';
+function properURL(role: Role, url: string) {
+  return role === Role.trainer
+    ? _maintrainer + '/' + url
+    : _mainUser + '/' + url;
+}
 export const _UserSideboard = [
   {
     name: 'Account',
     icon: 'fas fa-user-circle',
-    route: _editAccount,
+    route: properURL(Role.user, _editAccount),
   },
 ];
 export const _TrainerSideBoard = [
-  ..._UserSideboard,
+  {
+    name: 'Statics',
+    icon: 'fas fa-chart-bar',
+    route: properURL(Role.trainer, _dashboardStats_route),
+  },
+  {
+    name: 'Account',
+    icon: 'fas fa-user-circle',
+    route: properURL(Role.trainer, _editAccount),
+  },
   {
     name: 'Certifications',
     icon: 'fas fa-school',
-    route: _editCertification_route,
+    route: properURL(Role.trainer, _editCertification_route),
   },
   {
     name: 'Messages',
     icon: 'fas fa-envelope',
-    route: _myMessages_route,
-  },
-  {
-    name: 'Statics',
-    icon: 'fas fa-chart-bar',
-    route: _dashboardStats_route,
+    route: properURL(Role.trainer, _myMessages_route),
   },
 ];
