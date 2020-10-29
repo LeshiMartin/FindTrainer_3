60a718911a8ba174c812377d328796ded5f51229
eric-programming <eric09279@outlook.com>
2020-10-28 23:13:26 -0700

    updates single trainer to restart

--- client/src/app/pages/singletrainer/singletrainer.component.ts
+++ client/src/app/pages/singletrainer/singletrainer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TrainerDTO } from 'src/app/_model/_Dto/BaseUserDTO';
 import { UserService } from 'src/app/_services/user.service';
 @Component({
@@ -10,7 +10,7 @@ import { UserService } from 'src/app/_services/user.service';
   styleUrls: ['./singletrainer.component.scss'],
 })
 export class SingletrainerComponent implements OnInit {
-  currentTrainer: TrainerDTO;
+  currentTrainer: TrainerDTO = null;
   constructor(
     private modalService: NgbModal,
     private router: ActivatedRoute,
@@ -24,42 +24,9 @@ export class SingletrainerComponent implements OnInit {
   ngOnInit(): void {}
 
   getTrainer(id: string) {
-    this.userService.getSingleUser(id).subscribe((res: TrainerDTO) => this.currentTrainer = res);
+    this.userService
+      .getSingleUser(id)
+      .subscribe((res: TrainerDTO) => (this.currentTrainer = res));
   }
   /////////////////////////////////////////////////////////////////////
-  closeResult = '';
-  open(content) {
-    this.modalService
-      .open(content, { ariaLabelledBy: 'modal-basic-title' })
-      .result.then(
-        (result) => {
-          this.closeResult = `Closed with: ${result}`;
-        },
-        (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        }
-      );
-  }
-
-  rating(rate) {
-    this.modalService
-      .open(rate, { ariaLabelledBy: 'modal-basic-title' })
-      .result.then(
-        (result) => {
-          this.closeResult = `Closed with: ${result}`;
-        },
-        (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        }
-      );
-  }
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
 }
